77106733e9bd0ba2b9f6f8d5c269ebb7b540cd6b
Lauriene Florencio <lauriene.florencioambiental@gmail.com>
2026-01-06 18:24:18 +0000

    os cards estão sendo cortados na lateral, é necessário que não sejam cor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { initialLeads } from '@/lib/data';
 
 export default function Home() {
   return (
-    <div className='-m-4 sm:-m-6'>
-        <KanbanBoard initialLeads={initialLeads} />
+    <div className="-m-4 sm:-m-6">
+      <KanbanBoard initialLeads={initialLeads} />
     </div>
   );
 }
