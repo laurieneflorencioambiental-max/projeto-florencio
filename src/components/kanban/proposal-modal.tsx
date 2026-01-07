8140b6f35c43889b72eb14d51228884d95c34663
Lauriene Florencio <lauriene.florencioambiental@gmail.com>
2026-01-07 18:04:06 +0000

    coloque a parte branca até embaixo pois ficou esquisito

--- src/components/kanban/proposal-modal.tsx
+++ src/components/kanban/proposal-modal.tsx
@@ -287,13 +287,13 @@ export default function ProposalModal({
           </Select>
         </div>
 
-        <ScrollArea className="flex-1 bg-gray-100 dark:bg-gray-900 rounded-md">
+        <ScrollArea className="flex-1 bg-white dark:bg-gray-900 rounded-md">
           <div
             ref={proposalRef}
-            className="p-0 bg-white"
+            className="p-0"
             id="proposal-container"
           >
-            <div className="a4-page p-8 text-sm min-h-full" style={{ color: '#596371' }} id="proposal-content">
+            <div className="a4-page p-8 text-sm" style={{ color: '#596371', minHeight: '100%' }} id="proposal-content">
               {/* Cabeçalho da Proposta */}
               <header className="flex justify-between items-center pb-4 border-b">
                 <div>
