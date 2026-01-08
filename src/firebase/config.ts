0ec06550965b682040008e7858fbf0c8aaebbdf3
Lauriene Florencio <lauriene.florencioambiental@gmail.com>
2026-01-08 17:46:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4254367347-1a796",
+  "appId": "1:815408673372:web:44b8bdab1310fa06b2ec99",
+  "apiKey": "AIzaSyChDgB-5mNw0Fpea4pTRNW3jhtuKiYSfi0",
+  "authDomain": "studio-4254367347-1a796.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "815408673372"
+};
