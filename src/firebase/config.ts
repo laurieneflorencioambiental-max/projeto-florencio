0121bc06cd1ea86b0a849f47407f0fbb78899c07
Lauriene Florencio <lauriene.florencioambiental@gmail.com>
2026-01-20 19:50:21 +0000

    deu esse erro

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   projectId: "studio-4254367347-1a796",
   appId: "1:815408673372:web:44b8bdab1310fa06b2ec99",
-  apiKey: "AIzaSyChDgB-5mNwOFpea4pTRNW3jhtuKIYSf10",
+  apiKey: "AIzaSyChDgB-5mNw0Fpea4pTRNW3jhtuKiYSfi0",
   authDomain: "studio-4254367347-1a796.firebaseapp.com",
   storageBucket: "studio-4254367347-1a796.appspot.com",
   messagingSenderId: "815408673372"
