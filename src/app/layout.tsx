0ae00ad404652762345c7fdd0598cbae290565c4
Lauriene Florencio <lauriene.florencioambiental@gmail.com>
2026-02-02 18:01:43 +0000

    Onde esta a opção de instalar?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import type { ReactNode } from 'react';
@@ -71,7 +72,7 @@ export default function RootLayout({
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#1b7689" />
-        <link rel="manifest" href="/api/manifest.webmanifest" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
